c60f901585c9db2824c0dfba4300bbb72036ca35
Pam <pamhome21@gmail.com>
2023-05-23 16:28:52 +0500

    Task 9 Modules

---
+++ presentation/src/main/java/com/pamparamm/habittracker/presentation/ui/pages/AboutPageView.kt
@@ -0,0 +1,21 @@
+package com.pamparamm.habittracker.presentation.ui.pages
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.pamparamm.habittracker.presentation.BuildConfig
+import com.pamparamm.habittracker.presentation.R
+
+@Composable
+fun AboutPageView() {
+    Column {
+        Text(stringResource(R.string.app_name))
+        Divider()
+        Text(
+            "${stringResource(R.string.about_version_name)} : ${BuildConfig.VERSION_NAME}" +
+                    "\n${stringResource(R.string.about_version_code)} : ${BuildConfig.VERSION_CODE}"
+        )
+    }
+}
